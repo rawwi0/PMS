999e2f9f53cf7360ef58b59cd664dd2716c35bd3
rawwi0 <rkbaraiya04@gmail.com>
2026-06-13 13:01:21 +0530

    remove unused code

--- backend/src/app.js
+++ backend/src/app.js
@@ -31,34 +31,3 @@ app.use("/api/tasks", taskRoutes);
 app.use(errorHandler);
 
 export default app;
-
-// import express from "express";
-// import cors from "cors";
-// import morgan from "morgan";
-// import swaggerUi from "swagger-ui-express";
-// import swaggerSpec from "./docs/swagger.js";
-// import userRoutes from "./modules/user/user.routes.js";
-
-
-// import { errorHandler } from "./middlewares/error.middleware.js";
-
-
-// const app = express(); // ✅ FIRST create app
-
-// // Middlewares
-// app.use(cors());
-// app.use(express.json());
-// app.use(morgan("dev"));
-// app.use(errorHandler);
-// // Swagger
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-// // Routes
-// app.use("/api/users", userRoutes); // ✅ now it's safe
-
-// // Test route
-// app.get("/", (req, res) => {
-//   res.send("API is running 🚀");
-// });
-
-// export default app;
